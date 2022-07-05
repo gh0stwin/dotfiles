ae9a3874c9f1dd2f044ee977c2117e8401a2ff3c
Fábio Vital <fabiovital@tecnico.ulisboa.pt>
2022-07-05 18:46:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cp .p10k.zsh
+cp .p10k.zsh $HOME
 cp .profile $HOME
 cp .vimrc $HOME
 cp .zshrc $HOME
