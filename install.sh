9a8fd7c4f5e81e353996cfe594f1fb604d492e81
Fábio Vital <fabiovital@tecnico.ulisboa.pt>
2022-12-05 20:40:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cp .p10k.zsh $HOME
