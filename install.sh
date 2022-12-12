2b7a8292a0a257b31e3030e58c5108f216cd40b8
Fábio Vital <fabiovital@tecnico.ulisboa.pt>
2022-12-12 13:38:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cp -rf $HOME/.dotfiles/.p10k.zsh $HOME
-cp -rf $HOME/.dotfiles/.profile $HOME
-cp -rf $HOME/.dotfiles/.vimrc $HOME
-cp -rf $HOME/.dotfiles/.zshrc $HOME
+mkdir -p $HOME/.vim/backup
+mkdir -p $HOME/.vim/swap
+mkdir -p $HOME/.vim/undo
+
+cp -rf .p10k.zsh $HOME
+cp -rf .profile $HOME
+cp -rf .vimrc $HOME
+cp -rf .zshrc $HOME
